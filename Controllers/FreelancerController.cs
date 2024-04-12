3d432c6fc36fba43b03995e75e1f6b80a2c0c61c
Fatimah Zaim <105259691+Fatimahzaim99@users.noreply.github.com>
2024-04-12 12:39:06 +0800

    Add files via upload

---
+++ Controllers/FreelancerController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using webapiCDN.Models;
+
+namespace webapiCDN.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class FreelancerController : ControllerBase
+    {
+        private readonly FreelancerRepository _freelancerRepository;
+
+        public FreelancerController(FreelancerRepository freelancerRepository)
+        {
+            _freelancerRepository = freelancerRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Freelancer>>> GetAllFreelancers()
+        {
+            var freelancers = await _freelancerRepository.GetAllFreelancers();
+            return Ok(freelancers);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Freelancer>> GetFreelancerById(int id)
+        {
+            var freelancer = await _freelancerRepository.GetFreelancerById(id);
+            if (freelancer == null)
+            {
+                return NotFound();
+            }
+            return freelancer;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Freelancer>> AddUser(Freelancer freelancer)
+        {
+            await _freelancerRepository.AddFreelancer(freelancer);
+            return CreatedAtAction(nameof(GetFreelancerById), new { id = freelancer.Id }, freelancer);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateFreelancer(int id, Freelancer freelancer)
+        {
+            if (id != freelancer.Id)
+            {
+                return BadRequest();
+            }
+
+            await _freelancerRepository.UpdateFreelancer(freelancer);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFreelancer(int id)
+        {
+            await _freelancerRepository.DeleteFreelancer(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Controllers/FreelancerRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using webapiCDN.Models;
+
+namespace webapiCDN.Controllers
+{
+    public class FreelancerRepository
+    {
+        private readonly TodoContext _context;
+
+        public FreelancerRepository(TodoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Freelancer>> GetAllFreelancers()
+        {
+            return await _context.Freelancers.ToListAsync();
+        }
+
+        public async Task<Freelancer> GetFreelancerById(int id)
+        {
+            return await _context.Freelancers.FindAsync(id);
+        }
+
+        public async Task AddFreelancer(Freelancer freelancer)
+        {
+            _context.Freelancers.Add(freelancer);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateFreelancer(Freelancer freelancer)
+        {
+            _context.Entry(freelancer).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteFreelancer(int id)
+        {
+            var user = await _context.Freelancers.FindAsync(id);
+            _context.Freelancers.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
