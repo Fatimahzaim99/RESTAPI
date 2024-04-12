f0bdd3bf304e3c1ad0587231b949c525ec8ba70d
Fatimah Zaim <105259691+Fatimahzaim99@users.noreply.github.com>
2024-04-12 11:17:11 +0800

    Add files via upload

---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class User
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Username { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Mail { get; set; }
+
+    [Required]
+    [Phone]
+    public string PhoneNumber { get; set; }
+
+    [Required]
+    public string Skillsets { get; set; }
+
+    public string Hobby { get; set; }
+}
